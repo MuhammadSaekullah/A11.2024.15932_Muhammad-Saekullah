e01df4cef6f06be29ea575e2bef03c28e6439c3d
Muhammad Saekullah <111202415932@mhs.dinus.ac.id>
2026-04-22 19:39:20 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,10 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('/produk', 'ProdukController::index');
 $routes->get('/keranjang', 'TransaksiController::index');
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
+
+$routes->get('produk', 'ProdukController::index');
+$routes->get('keranjang', 'TransaksiController::index');
