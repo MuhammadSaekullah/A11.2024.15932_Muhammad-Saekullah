e4ca87d0915a75e258a2a529dc29a58bc8a4b7f4
Muhammad Saekullah <111202415932@mhs.dinus.ac.id>
2026-07-02 16:19:29 +0700

    belajar-ci

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,3 +35,5 @@ $routes->get('history', 'TransaksiController::history', ['filter' => 'auth']);
 $routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
 $routes->post('keranjang/get_ongkir', 'TransaksiController::get_ongkir');
 $routes->get('ajax/costs','TransaksiController::costs', ['filter' => 'auth']);
+$routes->resource('api/products', ['controller' => 'Api\ProdukController']);
+$routes->get('api/transactions', 'Api\TransaksiController::index');
--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -3,34 +3,35 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
         <li class="nav-item">
-            <a class="nav-link <?php echo (uri_string() == '') ? "" : "collapsed" ?>" href="/">
+            <a class="nav-link <?php echo (uri_string() == '') ? "" : "collapsed" ?>" href="<?= base_url('/') ?>">
                 <i class="bi bi-grid"></i>
                 <span>Home</span>
             </a>
-        </li>
-               
-        <li class="nav-item">
-            <a class="nav-link <?php echo (uri_string() == 'keranjang') ? "" : "collapsed" ?>" href="keranjang">
+        </li><li class="nav-item">
+            <a class="nav-link <?php echo (uri_string() == 'keranjang') ? "" : "collapsed" ?>" href="<?= base_url('keranjang') ?>">
                 <i class="bi bi-cart-check"></i>
                 <span>Keranjang</span>
             </a>
-        </li>
-        <?php if (session()->get('role') == 'admin') : ?>
+        </li><?php if (session()->get('role') == 'admin') : ?>
+            
             <li class="nav-item">
-                <a class="nav-link <?php echo (uri_string() == 'produk') ? "" : "collapsed" ?>" href="produk">
+                <a class="nav-link <?php echo (uri_string() == 'produk') ? "" : "collapsed" ?>" href="<?= base_url('produk') ?>">
                     <i class="bi bi-receipt"></i>
                     <span>Produk</span>
                 </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
+            </li><li class="nav-item">
+                <a class="nav-link <?php echo (uri_string() == 'history') ? "" : "collapsed" ?>" href="<?= base_url('history') ?>">
+                    <i class="bi bi-clock-history"></i>
+                    <span>History</span>
+                </a>
+            </li><li class="nav-item">
+                <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="<?= base_url('profile') ?>">
                     <i class="bi bi-person"></i>
                     <span>Profile</span>
                 </a>
-            </li> 
-        <?php endif; ?>   
-
+            </li><?php endif; ?>   
+        
     </ul>
 
-</aside>
+</aside>```
 
